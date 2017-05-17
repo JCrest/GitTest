9e25660831157879da32f406eb9889eedd42158d
JCrest <1554687045@qq.com>
2017-05-17 16:45:07 +0800

    上传3,新添加了一个类

---
+++ app/src/main/java/com/example/jiangchuanfa/gittest/NewJava.java
@@ -0,0 +1,16 @@
+package com.example.jiangchuanfa.gittest;
+
+/**
+ * Created by crest on 2017/5/17.
+ */
+
+public class NewJava {
+
+    private int i;
+    private int h;
+    private int g;
+    private int f;
+    private int e;
+    private int t;
+
+}
