bdb7bb2e7835bd72d77217c31c4a6bb590721bb7
JCrest <1554687045@qq.com>
2017-05-17 18:45:04 +0800

    上传4，这是一个新的java文件

---
+++ app/src/main/java/com/example/jiangchuanfa/gittest/NewJava2.java
@@ -0,0 +1,23 @@
+package com.example.jiangchuanfa.gittest;
+
+import android.os.Handler;
+import android.view.View;
+
+/**
+ * Created by crest on 2017/5/17.
+ *
+ *
+ */
+
+public class NewJava2 {
+
+   private View view;
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+
+        }
+    };
+
+    private class Message {
+    }
+}
